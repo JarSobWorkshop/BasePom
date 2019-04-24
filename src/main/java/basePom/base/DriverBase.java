8ce8588033d9819b3c4007fb0f25c113e196bb9f
Jarek <jaroslaw.sobieszuk@soflab.pl>
2019-04-24 14:55:59 +0200

    Add maven configuration for jenkins

--- src/main/java/basePom/base/DriverBase.java
+++ src/main/java/basePom/base/DriverBase.java
@@ -3,10 +3,10 @@ package basePom.base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class DriverBase {
@@ -24,17 +24,27 @@ public class DriverBase {
         WebDriverManager.firefoxdriver().setup();
         drivers.set(new FirefoxDriver());
         break;
-      case ("iedriver"):
-        WebDriverManager.iedriver().setup();
-        drivers.set(new InternetExplorerDriver());
-        break;
-      case ("edge"):
-        WebDriverManager.edgedriver().setup();
-        drivers.set(new EdgeDriver());
-        break;
     }
     drivers.get().manage().window().maximize();
     drivers.get().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+  }
+
+  public static void main(String[] args) {
+    Map<String, Integer> map1 = new HashMap<>();
+    Map<String, Integer> map2 = new HashMap<>();
+    Integer sum = 0;
+    Integer delta = 2;
+    map1.put("x", 1);
+    map1.put("y", 4);
+    map2.put("x", 3);
+    map2.put("y", 9);
+    for (String s : map1.keySet()) {
+      if (map1.containsKey(s) == map2.containsKey(s)) {
+        sum = Math.abs(map1.get(s) - map2.get(s));
+        System.out.println(sum);
+      }
+    }
 
   }
 
